6050cfb046f67984b4fcc63d907ecd88c747da49
Deep Chauhan <43372602+deepchauhan@users.noreply.github.com>
2022-03-03 16:42:12 +0530

    Add files via upload

---
+++ portfolio/src/back.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Back=()=>{
+    return (
+        <h1>back works</h1>);
+
+    };
+
+
+export default Back;
---
+++ portfolio/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import Back from './back'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <React.Fragment>
+    <App/>
+  </React.Fragment>,
+  rootElement
+)
